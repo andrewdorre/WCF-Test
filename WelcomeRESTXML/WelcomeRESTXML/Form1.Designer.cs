37bb2fe2cabcb9cabba6eb661b4beef976be2e9e
Andrew Dorre <andrewdorre@gmail.com>
2018-12-04 07:07:51 -0700

    Test WCF

---
+++ WelcomeRESTXML/WelcomeRESTXML/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WelcomeRESTXML
+{
+    partial class WelcomeRESTXML
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(12, 13);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(108, 16);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Enter your name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(126, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(84, 44);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // WelcomeRESTXML
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 79);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Name = "WelcomeRESTXML";
+            this.Text = "Welcome Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ WelcomeRESTXML/WelcomeRESTXML/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WelcomeRESTXML
+{
+    public partial class WelcomeRESTXML : Form
+    {
+        private HttpClient client = new HttpClient();
+
+        private XNamespace xmlNamespace = XNamespace.Get(
+            "http://schemas.microsoft.com/2003/10/Serialization/");
+
+        public WelcomeRESTXML()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string result = await client.GetStringAsync(new Uri(
+                "http://localhost:56021/WelcomeRESTXMLService.svc/welcome/" +
+                txtName.Text));
+
+            XDocument xmlResponse = XDocument.Parse(result);
+
+            MessageBox.Show(xmlResponse.Element(
+                xmlNamespace + "string").Value, "Welcome");
+        }
+    }
+}
